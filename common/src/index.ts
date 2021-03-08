2d63c9c8baa4c6bd66295454ae3552da5d393851
mroobert <robertmihairo94@gmail.com>
2021-03-08 21:18:01 +0200

    common - v1.0.8 - not found error fix

--- common/src/index.ts
+++ common/src/index.ts
@@ -3,6 +3,7 @@ export * from './errors/custom-error'
 export * from './errors/database-connection-error'
 export * from './errors/not-authorized-error'
 export * from './errors/route-not-found-error'
+export * from './errors/not-found-error'
 export * from './errors/request-validation-error'
 
 export * from './middlewares/current-user'
